3dd2cbaab9c5bd0d4e1785a117cc0c85c0543c57
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-07-13 19:01:49 +0530

    Adding multiple contacts to the address book

--- addressbook/AddressBook.java
+++ addressbook/AddressBook.java
@@ -6,10 +6,38 @@ public class AddressBook {
 	public static void main(String[] args) {
 
         System.out.println("***** WELCOME TO ADDRESS BOOK *****");
+        
+        System.out.println("---Welcome to Address Book---");
+        Scanner sc = new Scanner(System.in);
         System.out.println();
-        AddressBookService.addContact();
-        AddressBookService.display();
-        AddressBookService.editContact();
-        AddressBookService.deleteContact();
+        String option;
+        while(true) {
+            System.out.println(" ---- MENU ----");
+            System.out.println(" 1. Add contacts\n 2. Display contacts\n 3. Edit contacts\n 4. Delete contact\n 5. Exit");
+            option = sc.next();
+
+            switch (option) {
+                case "1":
+                    AddressBookService.addContact();
+                    break;
+                case "2":
+                    AddressBookService.display();
+                    break;
+                case "3":
+                    AddressBookService.editContact();
+                    break;
+                case "4":
+                    AddressBookService.deleteContact();
+                    break;
+                case "5":
+                    System.out.println("Thank You!");
+                    return;
+                default:
+                    System.out.println("Please enter a valid choice: ");
+            }
+
+        }
+
+
     }
 }
