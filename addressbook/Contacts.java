2b72ba688aaf9cfad60ebd1c3a22a68899dee9a7
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-07-13 19:44:04 +0530

    Search Person with address or city

--- addressbook/Contacts.java
+++ addressbook/Contacts.java
@@ -4,6 +4,19 @@ public class Contacts {
 	private String firstName;
     private String lastName;
     private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     private String city;
     private String state;
     private int zip;
@@ -29,6 +42,8 @@ public class Contacts {
     public String toString()
     {
         return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
-                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email;
+                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email+"\n";
     }
-}
+    
+    
+    }
