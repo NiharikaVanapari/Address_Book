a3547c6fef7834604069ab52617a9ccffc404567
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-07-13 19:47:44 +0530

    View the person searched from address book

--- addressbook/Contacts.java
+++ addressbook/Contacts.java
@@ -1,49 +1,47 @@
 package addressbook;
 
 public class Contacts {
-	private String firstName;
-    private String lastName;
-    private String address;
+	 private String firstName;
+	    private String lastName;
+	    private String address;
 
-    public String getCity() {
-        return city;
-    }
+	    public String getCity() {
+	        return city;
+	    }
 
-    public String getState() {
-        return state;
-    }
+	    public String getState() {
+	        return state;
+	    }
 
-    public String getLastName() {
-        return lastName;
-    }
+	    public String getLastName() {
+	        return lastName;
+	    }
 
-    private String city;
-    private String state;
-    private int zip;
-    private long phone;
-    private String email;
+	    private String city;
+	    private String state;
+	    private int zip;
+	    private long phone;
+	    private String email;
 
-    public String getFirstName() {
-        return firstName;
-    }
+	    public String getFirstName() {
+	        return firstName;
+	    }
 
-    public Contacts(String first_name, String last_name, String address, String city, String state, int zip, long phone, String email_id) {
-        this.firstName = first_name;
-        this.lastName = last_name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email_id;
-    }
+	    public Contacts(String first_name, String last_name, String address, String city, String state, int zip, long phone, String email_id) {
+	        this.firstName = first_name;
+	        this.lastName = last_name;
+	        this.address = address;
+	        this.city = city;
+	        this.state = state;
+	        this.zip = zip;
+	        this.phone = phone;
+	        this.email = email_id;
+	    }
 
-    @Override
-    public String toString()
-    {
-        return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
-                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email+"\n";
-    }
-    
-    
-    }
+	    @Override
+	    public String toString()
+	    {
+	        return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
+	                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email+"\n";
+	    }
+}
